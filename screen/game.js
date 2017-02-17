8af442195f53cc1b3d6c5633a6d3fd43bb341e10
Vincent Pasquier <vincentpasquier@posteo.net>
2017-02-17 09:54:29 +0100

    ..

--- screen/game.js
+++ screen/game.js
@@ -1,6 +1,7 @@
 var game,
 	players,
 	trump,
+	blocks,
 	walls,
 	platforms;
 
@@ -16,6 +17,7 @@ function preload() {
 	game.load.image('background', 'assets/background.png');
 	game.load.image('ground', 'assets/ground.png');
 	game.load.image('wall', 'assets/wall.png');
+	game.load.image('block', 'assets/block.png');
 	game.load.image('mexican', 'assets/mexican.png');
 	game.load.image('trump', 'assets/trump.png');
 }
@@ -41,15 +43,18 @@ function create() {
 		'mexican'));
 	players.push(game.add.sprite(380, game.world.height - 350,
 		'mexican'));
-	game.physics.arcade.enable(players);
 
 	trump = game.add.sprite(80, 0, 'trump');
 	game.physics.arcade.enable(trump);
 	trump.velocity = {x: 250, y: 0};
 	trump.body.velocity.x = trump.velocity.x;
+	trump.block = game.add.group();
+	getRandomBlock(trump.block);
+	console.log(trump.block);
 
 	for (var p in players) {
-		player = players[p];
+		var player = players[p];
+		game.physics.arcade.enable(player);
 		player.body.gravity.y = 600;
 		player.body.collideWorldBounds = true;	
 		player.velocity = {x: 150, y: 0};
@@ -61,6 +66,7 @@ function create() {
 function update() {
 	updatePlayers();
 	updateTrump();
+	updateBlock();
 }
 
 function updatePlayers () {
@@ -84,6 +90,76 @@ function updateTrump () {
 		trump.velocity.x *= -1;
 		trump.body.velocity.x = trump.velocity.x;
 	}
+}
+
+function updateBlock () {
+	
+	console.log(trump);
+	trump.block.x = trump.body.x;
+}
+
+function getRandomBlock(group) {
+		var bSize = 80;
+
+		var nOfBlocks = Math.floor((Math.random()*4)+1);
+		var version3 = Math.floor((Math.random()*2)+1);
+		var version4 = Math.floor((Math.random()*7)+1);
+
+		for (var i = 0; i<nOfBlocks*80; i+=80){
+			group.create(0+i, 150, 'block');
+		}
+		if(nOfBlocks == 3 && version3 == 2){
+			group.xy(0,300,50);
+			group.xy(1,380,50);
+			group.xy(2,380,130);
+		}
+		if(nOfBlocks==4){
+			switch(version4){
+				case 1:
+					group.xy(0,250,50);
+					group.xy(1,330,50);
+					group.xy(2,410,50);
+					group.xy(3,330,130);
+					break;
+				case 2:
+					group.xy(0,250,50);
+					group.xy(1,330,50);
+					group.xy(2,410,50);
+					group.xy(3,250,130);
+					break;
+				case 3:
+					group.xy(0,250,50);
+					group.xy(1,330,50);
+					group.xy(2,410,50);
+					group.xy(3,410,130);
+					break;
+				case 4:
+					group.xy(0,250,50);
+					group.xy(1,330,50);
+					group.xy(2,330,130);
+					group.xy(3,410,130);
+					break;
+				case 5:
+					group.xy(0,250,130);
+					group.xy(1,330,130);
+					group.xy(2,330,50);
+					group.xy(3,410,50);
+					break;
+				case 6:
+					group.xy(0,250,50);
+					group.xy(1,330,50);
+					group.xy(2,250,130);
+					group.xy(3,330,130);
+					break;
+				default:
+					break;
+			}
+		}
+		
+		group.setAll('enableBody', true);
+		game.physics.arcade.enable(group);
+
+		return;
 }
 
 function render() {
